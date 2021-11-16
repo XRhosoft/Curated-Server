cf8777234b27e70239caa679df97dedb8955a63f
Jacen <jbwyke@yahoo.com>
2021-11-15 17:38:11 -0700

    Initial commit

---
+++ src/main/java/com/xrhosoft/server/CuratedApplication.java
@@ -0,0 +1,13 @@
+package com.xrhosoft.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuratedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuratedApplication.class, args);
+	}
+
+}
